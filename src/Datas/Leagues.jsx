8e5665ba536f650b996abccd5e3c2e7f33962b45
bruce <bruce.monnerville@hotmail.fr>
2024-09-03 19:55:55 +0200

    ajout de nouvelles ligues

--- src/Datas/Leagues.jsx
+++ src/Datas/Leagues.jsx
@@ -16,7 +16,6 @@ export const leagues = [
         "logo": "https://media.api-sports.io/football/leagues/39.png",
         "flag": "https://media.api-sports.io/flags/gb.svg",
         "season": 2024,
-        "API": "https://v3.football.api-sports.io/fixtures?league=39&season=2024"
         },
 
         
@@ -27,7 +26,32 @@ export const leagues = [
                 "logo": "https://media.api-sports.io/football/leagues/140.png",
                 "flag": "https://media.api-sports.io/flags/es.svg",
                 "season": 2024,
-                "API": "https://v3.football.api-sports.io/fixtures?league=140&season=2024"
-                }
+                },
+
+            {
+                "id": 78,
+                "name": "Bundesliga",
+                "country": "Germany",
+                "logo": "https://media.api-sports.io/football/leagues/78.png",
+                "flag": "https://media.api-sports.io/flags/de.svg",
+                "season": 2024
+                },
+
+            {
+                "id": 135,
+                "name": "Serie A",
+                "country": "Italy",
+                "logo": "https://media.api-sports.io/football/leagues/135.png",
+                "flag": "https://media.api-sports.io/flags/it.svg",
+                "season": 2024,
+            },
+
+            {
+                "id": 2,
+               "name": "UEFA Champions League",
+                "country": "World",
+                "logo": "https://media.api-sports.io/football/leagues/2.png",
+                "season": 2024,
+            }
         
     ]
