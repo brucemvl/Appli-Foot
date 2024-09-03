0c52618d105970cb35bf5f521f818323636f2ca6
bruce <bruce.monnerville@hotmail.fr>
2024-09-03 15:14:03 +0200

    couleurs score

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -2,16 +2,21 @@ import "../styles/Match.scss"
 
  function Match({equipeDom, equipeExt, logoDom, logoExt, scoreDom, scoreExt}){
 
+ 
 
 
 return (
     <li className="match">
       <p className="match__equipeDom">{equipeDom}</p>
       <img className="match__logoDom" src={logoDom} alt="logo domicile" />
+      { scoreDom === scoreExt ? <div className="match__score">
+        <span className="nul">{scoreDom}</span>
+        <span className="nul">{scoreExt}</span>
+      </div> :
       <div className="match__score">
-        <span className="match__score__dom">{scoreDom}</span>
-        <span className="match__score__ext">{scoreExt}</span>
-      </div>
+        { scoreDom > scoreExt ? <span className="winner">{scoreDom}</span> : <span className="looser">{scoreDom}</span>}
+       { scoreExt > scoreDom ? <span className="winner">{scoreExt}</span> : <span className="looser">{scoreExt}</span>}
+      </div>}
       <img className="match__logoExt" src={logoExt} alt="logo exterieur" />
       <p className="match__equipeExt">{equipeExt}</p>
 
