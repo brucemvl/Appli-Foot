7bde005980ce2164865f7eac1a8b2397ff84bcf6
bruce <bruce.monnerville@hotmail.fr>
2024-10-15 22:39:10 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import "../styles/Footer.scss"
 import { Link } from "react-router-dom"
+import bm from "../assets/bm.png"
 
 function Footer(){
     return (
         <footer>
-            <Link to="/contact">Me contacter</Link>
+            <Link to="/contact">Nous contacter</Link>
+            <div><img src={bm} alt="logo"/><p>© 2024 BM Development. Tous droits réservés.</p></div>
 
 
         </footer>
